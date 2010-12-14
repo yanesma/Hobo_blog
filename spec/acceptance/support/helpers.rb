828c9ea509197d45635e0aad40a1545ef701472b
sense <sense@zeus.(none)>
2010-12-14 10:14:14 +0100

    rspec ready

--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -1,5 +1,17 @@
 module HelperMethods
   # Put helper methods you need to be available in all tests here.
+
+  def logger
+    Rails::logger
+  end
+
+  def login_as(user)
+    visit '/login'
+    fill_in 'login', :with => user.email_address
+    fill_in 'password', :with => user.password
+    click_button 'Log in'
+  end
+  
 end
 
 Spec::Runner.configuration.include(HelperMethods)
--- test/unit/author_test.rb
+++ test/unit/author_test.rb
@@ -1,8 +1,17 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+ context "ActiveRecord" do
+    setup { Factory(:author)}
+    should validate_uniqueness_of( :name )
+    should have_many(:posts)
+    should validate_presence_of(:name)
+
   end
+  context "An Organization" do
+    setup { @author = Factory(:author)}
+    should "do something"
+    should "do anotherthing"
+  end
+
 end
